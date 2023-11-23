6c693e98c8a8de3ab6ab7949e7b86049f0e07b01
Abhineet Agarwal <abhineetagarwal@Abhineets-MacBook-Pro-2.local>
2023-11-22 21:00:52 -0800

    adding config files for decision tree and knn datasets

---
+++ experiments/evaluation/config/dt/datasets.py
@@ -0,0 +1,11 @@
+from typing import List
+import torch.utils.data as data
+import numpy as np
+import torch
+
+
+from mcllm.data.synthetic_dt import DTDataset
+
+
+DATALOADERS = [('DT', DTDataset(10, 10, 3, frac_nan_rand_mask_list = 0.1,frac_nan_col_mask_list = 0.1,frac_col_vs_random = 0.5))]
+    
